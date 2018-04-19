c601d279e56fae7fcce8af8fa39af0996bb38c4e
Leovi <leovi.nutakor@cgi.com>
2018-04-19 14:00:58 +0200

    Rest controller functional

--- src/main/java/IPINoteGods/IPINotes/Controller/SampleController.java
+++ src/main/java/IPINoteGods/IPINotes/Controller/SampleController.java
@@ -1,17 +1,42 @@
 package IPINoteGods.IPINotes.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import IPINoteGods.IPINotes.Model.Etudiant;
+import IPINoteGods.IPINotes.Service.EtudiantService;
+
+@RestController
 @RequestMapping("/")
 public class SampleController {
+	
+	@Autowired
+	EtudiantService etudiantService;
 
 	@RequestMapping("test")
-	@ResponseBody
-	public String testPage() {
-		return "test page";
+	public List<Etudiant> testPage() {
+		
+		spawnFakeEtudiants();
+		
+		List<Etudiant> etudiants = etudiantService.findAll();
+		
+		return etudiants;
+	}
+	
+	private void spawnFakeEtudiants() {
+		{
+			Etudiant hello = new Etudiant();
+			hello.setName("EtName");
+			etudiantService.save(hello);
+		}
+		{
+			Etudiant hello = new Etudiant();
+			hello.setName("EtName2");
+			etudiantService.save(hello);
+		}
 	}
 	
 }
