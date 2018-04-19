9316f1be5c8a8eddcb5a5eda4f221311f9dd35c7
yann.laforest <yann.laforest@cgi.com>
2018-04-19 12:28:29 +0200

    Initialize SpringBoot project

---
+++ ipi-note-server/src/main/java/com/ipinote/server/IpiNoteServerApplication.java
@@ -0,0 +1,12 @@
+package com.ipinote.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpiNoteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpiNoteServerApplication.class, args);
+	}
+}
