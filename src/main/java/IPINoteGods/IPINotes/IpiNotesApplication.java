bc0325fd5351a7c817d80c17d28df9f259332cee
Leovi <leovi.nutakor@cgi.com>
2018-04-19 12:23:51 +0200

    Imported spring initializr dl

---
+++ src/main/java/IPINoteGods/IPINotes/IpiNotesApplication.java
@@ -0,0 +1,12 @@
+package IPINoteGods.IPINotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpiNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpiNotesApplication.class, args);
+	}
+}
---
+++ src/test/java/IPINoteGods/IPINotes/IpiNotesApplicationTests.java
@@ -0,0 +1,16 @@
+package IPINoteGods.IPINotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpiNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
